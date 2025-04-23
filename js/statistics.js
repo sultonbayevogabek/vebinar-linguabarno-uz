8ae9d2a6c2b8cbd587730d978c7da83f2558eb86
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-04-23 22:51:27 +0500

    initial commit

--- js/statistics.js
+++ js/statistics.js
@@ -1,7 +1,7 @@
 'use strict';
 
 class Statistics {
-  PROJECT_NAME = 'vebinar-linguabarno-uz';
+  PROJECT_NAME = window.location.host.replace(/\./g, '-');
   DB_URL = 'https://webinar-pages-statistics-default-rtdb.firebaseio.com/';
   ENTERED_DB = `${this.DB_URL}${this.PROJECT_NAME}-entered.json`;
   CLICKED_REG_BTN_DB = `${this.DB_URL}${this.PROJECT_NAME}-clicked-reg-btn.json`;
